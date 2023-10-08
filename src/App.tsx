731089b9f0ba704631b8ab43447e0e19c9fa0e1a
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-08 17:45:28 -0300

    adicionando página principal no arquivo App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomeScreen from './src/screens/Home.page';
 
 function App() {
   return (
     <div className="App">
-     
+      <HomeScreen/>
     </div>
   );
 }
