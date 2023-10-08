c47dec825655e4230841a402bafa16eccb8a7c45
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-08 18:29:50 -0300

    adicionando tipagem

--- src/components/SidebarSection.component.tsx
+++ src/components/SidebarSection.component.tsx
@@ -1,6 +1,6 @@
 import '../styles/sidebar.css';
 
-export default function SidebarSectionComponent({children}){
+export default function SidebarSectionComponent(props: {children: React.ReactNode}){
     return(
         <div className="left-sidebar-section">
             {children}
