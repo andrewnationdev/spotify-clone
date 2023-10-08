011c973e03117cd2657f17f34939e3a11f02e9ff
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-08 20:39:51 -0300

    removendo margin e mexendo na header da biblioteca

--- src/components/LibrarySidebarHeader.component.tsx
+++ src/components/LibrarySidebarHeader.component.tsx
@@ -5,7 +5,7 @@ export default function LibrarySidebarHeaderComponent(){
     return(
         <div className="library-sidebar-header">
             <div className="library-header-title">
-                <i className="material-icons"></i>
+                <i className="material-icons">burst_mode</i>
                 <span>Sua Biblioteca</span>
             </div>
             <div className="library-header-icons-container">
