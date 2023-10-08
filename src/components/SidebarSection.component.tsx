ac4b6ffa5dfa16ab6f1f90b91cbd31419875c289
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-08 18:31:43 -0300

    adicionando props que faltavam

--- src/components/SidebarSection.component.tsx
+++ src/components/SidebarSection.component.tsx
@@ -3,7 +3,7 @@ import '../styles/sidebar.css';
 export default function SidebarSectionComponent(props: {children: React.ReactNode}){
     return(
         <div className="left-sidebar-section">
-            {children}
+            {props.children}
         </div>
     );
 }
