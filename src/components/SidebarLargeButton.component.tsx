ea3c2e54e569fe3134e974c0bfa656ff5aa44b23
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-08 19:16:30 -0300

    corrigindo tipagens

--- src/components/SidebarLargeButton.component.tsx
+++ src/components/SidebarLargeButton.component.tsx
@@ -1,11 +1,13 @@
+import '../styles/sidebar.css';
+
 export default function SidebarLargeButtonComponent(props: {
-    icon: string, "Home" "search"
-    title: string "Inicio" "buscar"
+    icon: string,
+    title: string
 }){
     return(
-        <>
-            <i class="material-icons">{props?.icon}</i>
+        <div className="sidebar-large-button">
+            <i className="material-icons">{props?.icon}</i>
             <span>{props?.title}</span>
-        </>
+        </div>
     );
 }
