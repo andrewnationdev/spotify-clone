26ce6c6eff5029b458e8ce1bee80e10293b304fa
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-09 13:50:52 -0300

    adicionando ícone

--- src/screens/Home.page.tsx
+++ src/screens/Home.page.tsx
@@ -2,17 +2,20 @@ import SidebarComponent from '../components/Sidebar.component';
 import '../styles/pages.css';
 import CircleDarkerButtonComponent from '../components/CircleDarkerButton.component';
 
-export default function HomeScreen(){
-    return(
+export default function HomeScreen() {
+    return (
         <div className="page-container">
             <header>
                 <div className="circle-darker-buttons-container">
-                    <CircleDarkerButtonComponent icon="keyboard_arrow_left"/>
-                    <CircleDarkerButtonComponent icon="keyboard_arrow_right"/>
+                    <CircleDarkerButtonComponent icon="keyboard_arrow_left" />
+                    <CircleDarkerButtonComponent icon="keyboard_arrow_right" />
                 </div>
                 <div className="right-header-container">
-                   <button className="pill-button-header white-pill">Ver planos Premium</button> 
-                   <button className="pill-button-header black-pill">Instalar aplicativo</button>
+                    <button className="pill-button-header white-pill">Ver planos Premium</button>
+                    <button className="pill-button-header black-pill">
+                        <i className="material-icon">get_app</i>
+                        Instalar aplicativo
+                    </button>
                 </div>
             </header>
         </div>
