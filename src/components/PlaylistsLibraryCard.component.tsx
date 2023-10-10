6c416e3af987b093b0a5255f1ef6981ef680003b
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-10 15:58:33 -0300

    mock de playlists

--- src/components/PlaylistsLibraryCard.component.tsx
+++ src/components/PlaylistsLibraryCard.component.tsx
@@ -12,8 +12,8 @@ export default function PlaylistsLibraryCardComponent(props: {
                 <img className="playlist-card-picture" src="https://th.bing.com/th/id/OIP.k8VgOI80DfmuekaKmRDY5wHaHa?pid=ImgDet&rs=1" alt="" />
             </div>
             <div className="playlist-card-info-container">
-                <span className="playlist-card-title">Imagine Dragons Best...</span>
-                <span className="playlist-card-description">Playlist</span>
+                <span className="playlist-card-title">{props?.title}</span>
+                <span className="playlist-card-description">Playlist • {props?.creator}</span>
             </div>
         </div>
     );
