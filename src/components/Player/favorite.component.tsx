04dfa64b3fe0f44f2b8a845b63d847b979da4de6
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-10 16:43:55 -0300

    trocndo cor conforme estado

--- src/components/Player/favorite.component.tsx
+++ src/components/Player/favorite.component.tsx
@@ -4,7 +4,9 @@ export default function FavoriteComponent(){
     const [selected, setSelected] = useState<boolean>(false);
 
     return(
-        <div className={""} onClick={() => setSelected(!selected)}>
+        <div className={""} styles={{
+            color: selected ? '' : ''
+        }} onClick={() => setSelected(!selected)}>
             <i className="material-icons">favorite</i>
         </div>
     );
