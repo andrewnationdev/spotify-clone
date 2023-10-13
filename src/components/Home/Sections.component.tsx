686f388821a4372c3067ccc2b3374013f8363849
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-13 12:43:35 -0300

    adicionando prop de título

--- src/components/Home/Sections.component.tsx
+++ src/components/Home/Sections.component.tsx
@@ -1,11 +1,14 @@
 import '../../styles/home.css';
 import SingleSectionComponent from './SingleSection.component';
-import {userMock} from '../../mock/userMock';
+import { userMock } from '../../mock/userMock';
 
-export default function SectionsComponent(){
-    return(
+export default function SectionsComponent() {
+    return (
         <div className="home-page-v">
-            <SingleSectionComponent data={userMock?.section[0]}/>
+            <SingleSectionComponent 
+                title={userMock?.section[0]?.title} 
+                data={userMock?.section[0]?.data} 
+            />
         </div>
     )
 }
