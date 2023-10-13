69d197ba27a869b42006db6f3a50e145a0f9b481
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-13 12:04:45 -0300

    adicionando dados que faltavam

--- src/components/Home/Sections.component.tsx
+++ src/components/Home/Sections.component.tsx
@@ -1,10 +1,11 @@
 import '../../styles/home.css';
 import SingleSectionComponent from './SingleSection.component';
+import {userMock} from '../mock/userMock';
 
 export default function SectionsComponent(){
     return(
         <div className="home-page-v">
-            <SingleSectionComponent/>
+            <SingleSectionComponent data={userMock?.section}/>
         </div>
     )
 }
