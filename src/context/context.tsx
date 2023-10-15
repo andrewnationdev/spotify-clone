8296d5fb65b758aed621d4e389195ec762a5fc19
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-15 12:24:18 -0300

    tipagem

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,21 +1,26 @@
 import React, { createContext, useContext, useState } from 'react';
-import { IAPIResponse } from '../types/API';
+import {IAPIResponse} from '../types/API';
 
-export const SpotifyContext = createContext<{ IAPIResponse, updateData: (x: any) => void } | {}>({});
+export const SpotifyContext = createContext<{
+    IAPIResponse;
+    updateData: (x: any) => void 
+} | {
+    updateData: (x: any) => void
+}>({});
 
 export const SpotifyProvider = ({ children }) => {
-    const [data, setData] = useState<IAPIResponse | {}>({});
+  const [data, setData] = useState<IAPIResponse | {}>({});
 
-    const updateData = (updatedData) => {
-        setData((prevUser) => ({
-            ...prevData,
-            ...updatedData,
-        }));
-    };
+  const updateData = (updatedData) => {
+    setData((prevUser) => ({
+      ...prevData,
+      ...updatedData,
+    }));
+  };
 
-    return (
-        <SpotifyContext.Provider value={{ user, updateUserData }}>
-            {children}
-        </SpotifyContext.Provider>
-    );
+  return (
+    <SpotifyContext.Provider value={{ user, updateUserData }}>
+      {children}
+    </SpotifyContext.Provider>
+  );
 };
