cbdba6089c947dae86ee09c342e2c4d350087d5e
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-15 14:28:58 -0300

    estado

--- src/modules/store.ts
+++ src/modules/store.ts
@@ -2,6 +2,6 @@ import { create } from 'zustand'
 
 const useSpotifyStore = create((set) => ({
   bears: 0,
-  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+  increasePopulation: () => set((state: number) => ({ bears: state.bears + 1 })),
   removeAllBears: () => set({ bears: 0 }),
 }))
