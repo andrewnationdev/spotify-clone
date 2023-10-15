51a26322276d9568f49e05d39e45c190a71d923c
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-15 13:00:48 -0300

    queryclient

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomeScreen from './screens/Home.page';
@@ -6,8 +6,8 @@ import SidebarComponent from './components/Sidebar.component';
 import PlayerComponent from './components/Player/Player.component';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import fetchAPIData from './modules/fetch';
-import {IAPIResponse} from './types/API';
-import {SpotifyProvider, SpotifyContext} from './context/context';
+import { IAPIResponse } from './types/API';
+import { SpotifyProvider, SpotifyContext } from './context/context';
 import { useQuery } from 'react-query';
 
 function App() {
@@ -16,25 +16,27 @@ function App() {
   const context = useContext(SpotifyContext);
 
   const fetchData = async () => {
-      const data = await fetchAPIData("ght123");
+    const data = await fetchAPIData("ght123");
 
-      context.updateData(data);
+    context.updateData(data);
   }
 
   const { data, isLoading, isError, refetch } = useQuery('global-query', fetchData);
 
   return (
-    <SpotifyProvider>
-      <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}>
+      <SpotifyProvider>
         <div className="App">
-          <div className="main-flex">
-            <SidebarComponent/>
-            <HomeScreen/>
-          </div>
-          <PlayerComponent/>
+          {!isLoading && <>
+            <div className="main-flex">
+              <SidebarComponent />
+              <HomeScreen />
+            </div>
+            <PlayerComponent />
+          </>}
         </div>
-      </QueryClientProvider>
-    </SpotifyProvider>
+      </SpotifyProvider>
+    </QueryClientProvider>
   );
 }
 
