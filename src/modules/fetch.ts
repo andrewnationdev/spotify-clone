c0b643d8e30f976288d150191bdc300ecfb5eeac
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-15 12:48:41 -0300

    retorno promise

--- src/modules/fetch.ts
+++ src/modules/fetch.ts
@@ -1,6 +1,6 @@
 import {IAPIResponse} from '../types/API';
 
-export default async function fetchAPIData(user: string):IAPIResponse {
+export default async function fetchAPIData(user: string): Promise<IAPIResponse> {
     const data = await fetch(`/users/${user}.json`);
     const res = await data.text();
 
